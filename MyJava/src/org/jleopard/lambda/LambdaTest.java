af437585418ef1d3fbe6073342f28e9d5acf3d7e
Chen_9g <502622717@qq.com>
2018-09-13 21:06:04 +0800

    lambda

--- MyJava/src/org/jleopard/lambda/LambdaTest.java
+++ MyJava/src/org/jleopard/lambda/LambdaTest.java
@@ -1,5 +1,8 @@
 package org.jleopard.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,14 +18,19 @@ public class LambdaTest{
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
+       /* for (int i = 0; i < 5; i++) {
             new Thread(()->System.out.println("线程1")).start();
             new Thread(()->System.out.println("线程2")).start();
             new Thread(()->System.out.println("线程3")).start();
         }
+*/
+       /* String a = "GG";
+        Stream.of(a).map(i-> i+"JJ").forEach(System.out::println);*/
 
-        String a = "GG";
-        Stream.of(a).map(i-> i+"JJ").forEach(System.out::println);
+        List<Integer> list = Arrays.asList(1,2,2,2,8,5,6,7,6,9);
+      //  list.stream().distinct().forEach(System.out::println);
+      //  list.stream().filter(i->i>5).forEach(System.out::println);
+        list.stream().flatMap(i->Arrays.asList(i).stream()).forEach(System.out::println);
     }
 
     public static String sayHello(String arg){
